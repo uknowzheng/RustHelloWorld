5537aa5f043a5f4c313e4a3a7e15f40fd633e7f3
uknowzheng <uknowzheng@tencent.com>
2021-06-14 13:44:04 +0800

    feat:first commit

---
+++ src/main.rs
@@ -0,0 +1,40 @@
+use std::fs;
+use std::io::prelude::{Read, Write};
+use std::net::{TcpListener, TcpStream};
+
+fn main() {
+  let listener = TcpListener::bind("127.0.0.1:7878").unwrap();
+  for stream in listener.incoming() {
+    let _stream = stream.unwrap();
+    handel_connection(_stream);
+  }
+}
+
+fn handel_connection(mut stream: TcpStream) {
+  let mut buffer = [0; 1024];
+  stream.read(&mut buffer).unwrap();
+  println!("Request: {}", String::from_utf8_lossy(&buffer[..]));
+  let get = b"GET / HTTP/1.1\r\n";
+  let (status_line, filename) = if buffer.starts_with(get) {
+    ("HTTP/1.1 200 OK", "hello.html")
+  } else {
+    ("HTTP/1.1 200 OK", "hello.html")
+  };
+
+  let contents = fs::read_to_string(filename).unwrap();
+  let response = format!(
+    "HTTP/1.1 200 OK\r\nContent-Length: {}\r\n\r\n{}",
+    contents.len(),
+    contents
+  );
+  stream.write(response.as_bytes()).unwrap();
+  stream.flush().unwrap();
+
+  let status_line = "HTTP/1.1 404 NOT FOUND\r\n\r\n";
+  let contents = fs::read_to_string("404.html").unwrap();
+
+  let response = format!("{}{}", status_line, contents);
+
+  stream.write(response.as_bytes()).unwrap();
+  stream.flush().unwrap();
+}
